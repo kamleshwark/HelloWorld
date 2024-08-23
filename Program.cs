216a1410b7003322cce324168d7f707c3f51d12f
kamleshwark <kamleshwark@gmail.com>
2024-08-23 18:28:56 +0530

    Name change

--- Program.cs
+++ Program.cs
@@ -26,7 +26,7 @@ app.MapGet("/weatherforecast", () =>
     var forecast =  Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
-            "Kamleshwar",
+            "Waether Forecast branch",
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             Random.Shared.Next(-20, 55),
             summaries[Random.Shared.Next(summaries.Length)]
